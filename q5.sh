4d7dd86132257adbf1cfec9a4e153b0cc5dba85c
sandip123pal <59143462+sandip123pal@users.noreply.github.com>
2023-07-26 13:58:52 +0530

    q6.sh

--- q5.sh
+++ q5.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-# Set the size of the chessboard (8x8 in this case)
-board_size=8
+# Function to display an error message and exit the program
+show_error_and_exit() {
+    echo "Error: $1"
+    exit 1
+}
 
-# Generate and display the chessboard
-for ((row = 1; row <= board_size; row++)); do
-    for ((col = 1; col <= board_size; col++)); do
-        if ((row % 2 == col % 2)); then
-            echo -n "  "  # White square
-        else
-            echo -n "##"  # Black square
-        fi
-    done
-    echo
+# Prompt the user to enter the name of a directory
+read -p "Enter the name of the directory: " directory_name
+
+# Check if the directory exists
+if [ ! -d "$directory_name" ]; then
+    show_error_and_exit "Directory '$directory_name' does not exist."
+fi
+
+# List all the files in the given directory and sort them alphabetically
+files=$(ls "$directory_name" | sort)
+
+# Create a new directory named "sorted" inside the given directory
+sorted_directory="$directory_name/sorted"
+mkdir -p "$sorted_directory"
+
+# Move each file from the original directory to the "sorted" directory
+num_files_moved=0
+for file in $files; do
+    mv "$directory_name/$file" "$sorted_directory/"
+    num_files_moved=$((num_files_moved + 1))
 done
+
+# Display a success message with the total number of files moved
+echo "Successfully moved $num_files_moved files to '$sorted_directory'."
